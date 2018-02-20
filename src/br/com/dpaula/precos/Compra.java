7686c565935dc14d051ed38f8e8d96a29c7e3a7b
dpaula <fernando.dpaula@gmail.com>
2018-02-20 09:27:45 -0300

    Incluida calculo de preços e suas regras de tabela e envio

---
+++ src/br/com/dpaula/precos/Compra.java
@@ -0,0 +1,15 @@
+package br.com.dpaula.precos;
+
+public class Compra {
+
+	public double getValor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getCidade() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
